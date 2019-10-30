702b4a38dfd329449edaccb77cdca0fea12d975b
Joel Estrella <joel.j8@gmail.com>
2019-10-30 17:22:49 -0400

    Fixed Api OpcionesSist

--- PVenta.Models/ApiModels/ApiOpcionesSist.cs
+++ PVenta.Models/ApiModels/ApiOpcionesSist.cs
@@ -9,10 +9,8 @@ namespace PVenta.Models.ApiModels
     public class ApiOpcionesSist
     {
         public string ID { get; set; }
-
         public string Codigo { get; set; }
         public string Descripcion { get; set; }
-
         public bool Inactivo { get; set; }
     }
 }
