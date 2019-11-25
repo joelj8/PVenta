73897c7261294180d9f03cc37511af9e4d107a64
Joel Estrella <joel.j8@gmail.com>
2019-11-25 14:04:08 -0400

    Update info DAL

---
+++ PVenta.DAL/Migrations/201911251800212_JEv023.cs
@@ -0,0 +1,20 @@
+﻿namespace PVenta.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class JEv023 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.FacturaDetails", "Orden", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+            AddColumn("dbo.OrderDetails", "Orden", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.OrderDetails", "Orden");
+            DropColumn("dbo.FacturaDetails", "Orden");
+        }
+    }
+}
