7141aacf60fa247542b7569f59a5cb236a37e48a
Joel Estrella <joel.j8@gmail.com>
2019-12-22 23:01:58 -0400

    Test in House

---
+++ PVenta.DAL/Migrations/201912221717479_JEv041.cs
@@ -0,0 +1,26 @@
+﻿namespace PVenta.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class JEv041 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.FacturaPayments", "MontoDevolver", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+            AddColumn("dbo.FacturaPayments", "FormaPagoId", c => c.String(maxLength: 50, unicode: false));
+            AddColumn("dbo.FacturaPayments", "InfoPago", c => c.String());
+            CreateIndex("dbo.FacturaPayments", "FormaPagoId");
+            AddForeignKey("dbo.FacturaPayments", "FormaPagoId", "dbo.FormaPagos", "ID");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.FacturaPayments", "FormaPagoId", "dbo.FormaPagos");
+            DropIndex("dbo.FacturaPayments", new[] { "FormaPagoId" });
+            DropColumn("dbo.FacturaPayments", "InfoPago");
+            DropColumn("dbo.FacturaPayments", "FormaPagoId");
+            DropColumn("dbo.FacturaPayments", "MontoDevolver");
+        }
+    }
+}
